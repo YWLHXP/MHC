57e18ea6430c5ff7b7e99674f45f9556e9520965
peng <xpdragon1993@163.com>
2016-07-06 17:04:17 +0800

    添加Bmob,完成登录注册功能

--- ManHuaCheng/ManHuaCheng/Classes/Mine(我的)/Controller/XPMyWordViewController.m
+++ ManHuaCheng/ManHuaCheng/Classes/Mine(我的)/Controller/XPMyWordViewController.m
@@ -8,8 +8,9 @@
 
 #import "XPMyWordViewController.h"
 
-@interface XPMyWordViewController ()
-
+@interface XPMyWordViewController ()<UITableViewDataSource,UITableViewDelegate>
+/** tableView */
+@property (nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation XPMyWordViewController
@@ -17,6 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"我的话题";
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.backgroundColor = [UIColor grayColor];
 }
 
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+{
+    return 0;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    return cell;
+}
 @end
